630eb77cc1030ed32f718870c5373fb823cb3ecd
Andrew <43748949+thecointrack@users.noreply.github.com>
2019-06-13 21:50:22 +0300

    clean

---
+++ clean.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Clean</title>
+	<style>
+		body {
+			background: yellow;
+		}
+		h1 {
+			color: white;
+			font-size: 14rem;
+		}
+	</style>
+</head>
+<body>
+	<h1>Идет чистка!</h1>
+</body>
+</html>
